9a8f2e6958c36c900c6d71b0289b6f57b91e98ea
TakutoKitabayashi <tk.yabashi@gmail.com>
2025-05-16 18:36:30 +0900

    シェア文言にブログ名と記事タグを追加

--- src/components/ShareButton.tsx
+++ src/components/ShareButton.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import type { SelectProperty } from '../lib/interfaces';
 
 import {
   TwitterShareButton, XIcon,
@@ -9,15 +10,22 @@ import {
 interface Props {
   url: string;
   title: string;
+  tags?: SelectProperty[];
 }
 
 export const SocialShareButtons: React.FC<Props> = (props) => {
-  const { url, title } = props
+  const { url, title, tags = [] } = props;
   const buttonStyle = {
     padding: "4px",
     margin: "4px",
     alignItems: "center"
   };
+
+  const generatedHashtags = tags
+    .map(tag => tag.name.replace(/\s+/g, ''))
+    .filter(tag => tag);
+
+  const allHashtags = ['あでぃの製作所', ...generatedHashtags];
 
   return (
     <>
@@ -29,7 +37,7 @@ export const SocialShareButtons: React.FC<Props> = (props) => {
         <FacebookIcon round size={40} />
         {/*<FacebookShareCount url={url} className="text-blue-600 font-semibold" />*/}
       </FacebookShareButton>
-      <TwitterShareButton url={url} title={title} style={buttonStyle}>
+      <TwitterShareButton url={url} title={title} hashtags={allHashtags} style={buttonStyle}>
         <XIcon size={40} round />
       </TwitterShareButton>
     </>
