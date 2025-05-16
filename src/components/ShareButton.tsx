02d36b1924ef9d8c8d2bcef33c99a90177d8d22c
TakutoKitabayashi <tk.yabashi@gmail.com>
2025-05-16 22:16:08 +0900

    ハッシュタグでのシェアを見据えてブログ名変更

--- src/components/ShareButton.tsx
+++ src/components/ShareButton.tsx
@@ -31,7 +31,7 @@ export const SocialShareButtons: React.FC<Props> = (props) => {
       .filter(tag => tag);
   }
 
-  const allHashtags = ['あでぃの〇〇製作所', 'blog', ...generatedHashtags];
+  const allHashtags = ['あでぃ工房', 'blog', ...generatedHashtags];
 
   return (
     <>
